ba09e2f51fe228a6424baed8beda92cc7e3d9d02
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-09-25 19:20:49 +0000

    fix test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_database/trunk@73680 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_database/tests/__init__.py
+++ src/nti/analytics_database/tests/__init__.py
@@ -15,6 +15,33 @@ from sqlalchemy import create_engine as sqlalchemy_create_engine
 
 from nti.analytics_database import Base
 
+# import in order to create tables
+from nti.analytics_database.assessments import AssignmentViews
+
+from nti.analytics_database.blogs import BlogsCreated
+
+from nti.analytics_database.boards import TopicsViewed
+
+from nti.analytics_database.enrollments import CourseCatalogViews
+
+from nti.analytics_database.profile_views import EntityProfileViews
+
+from nti.analytics_database.resource_tags import NotesCreated
+
+from nti.analytics_database.resource_views import VideoEvents
+
+from nti.analytics_database.resources import Resources
+
+from nti.analytics_database.root_context import RootContextId
+
+from nti.analytics_database.sessions import Sessions
+
+from nti.analytics_database.social import ChatsJoined
+
+from nti.analytics_database.surveys import SurveysTaken
+
+from nti.analytics_database.users import Users
+
 def create_engine(dburi='sqlite://', pool_size=30, max_overflow=10, pool_recycle=300):    
     try:
         if dburi == 'sqlite://':
@@ -45,7 +72,8 @@ class AnalyticsDatabaseTest(unittest.TestCase):
     
     def setUp(self):
         self.engine = create_engine()
-        self.metadata = getattr(Base, 'metadata').create_all(self.engine)
+        self.metadata = getattr(Base, 'metadata')
+        self.metadata.create_all(bind=self.engine)
         self.sessionmaker = create_sessionmaker(self.engine)
         self.session = create_session(self.sessionmaker)
 
--- src/nti/analytics_database/tests/test_database.py
+++ src/nti/analytics_database/tests/test_database.py
@@ -7,17 +7,12 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from hamcrest import is_
 from hamcrest import has_length
 from hamcrest import assert_that
-
-from nti.analytics_database.users import Users
 
 from nti.analytics_database.tests import AnalyticsDatabaseTest
 
 class TestDatabase(AnalyticsDatabaseTest):
 
     def test_database(self):
-        count = self.session.query(Users).count()
-        assert_that(count, is_(0))
-        assert_that(self.engine.table_names(), has_length(1))
+        assert_that(self.engine.table_names(), has_length(61))
