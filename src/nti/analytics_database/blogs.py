c425d169c9644340c5f62ee2974e8ad8b7ab73c0
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-20 22:19:06 +0000

    pylint
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_database/trunk@115654 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_database/blogs.py
+++ src/nti/analytics_database/blogs.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -22,6 +22,10 @@ from sqlalchemy.schema import PrimaryKeyConstraint
 
 from zope import component
 
+from nti.analytics_database import INTID_COLUMN_TYPE
+
+from nti.analytics_database import Base
+
 from nti.analytics_database.interfaces import IAnalyticsIntidIdentifier
 
 from nti.analytics_database.meta_mixins import CreatorMixin
@@ -33,113 +37,130 @@ from nti.analytics_database.meta_mixins import BaseTableMixin
 from nti.analytics_database.meta_mixins import TimeLengthMixin
 from nti.analytics_database.meta_mixins import FileMimeTypeMixin
 
-from nti.analytics_database import Base
-from nti.analytics_database import INTID_COLUMN_TYPE
-
 from nti.property.property import alias
+
 
 class BlogMixin(object):
 
-	@declared_attr
-	def blog_id(cls):
-		return Column('blog_id', Integer, ForeignKey("BlogsCreated.blog_id"), nullable=False, index=True)
+    @declared_attr
+    def blog_id(self):
+        return Column('blog_id', Integer, ForeignKey("BlogsCreated.blog_id"),
+                      nullable=False, index=True)
 
-	@declared_attr
-	def _blog_record(self):
-		return relationship( 'BlogsCreated', lazy="select", foreign_keys=[self.blog_id] )
+    @declared_attr
+    def _blog_record(self):
+        return relationship('BlogsCreated', lazy="select",
+                            foreign_keys=[self.blog_id])
 
-	@property
-	def Blog(self):
-		return self._blog_record.Blog
+    @property
+    def Blog(self):
+        return self._blog_record.Blog
+
 
 class BlogsCreated(Base, BaseTableMixin, DeletedMixin, RatingsMixin):
 
-	__tablename__ = 'BlogsCreated'
+    __tablename__ = 'BlogsCreated'
 
-	BlogLength = alias( 'blog_length' )
+    BlogLength = alias('blog_length')
 
-	blog_ds_id = Column('blog_ds_id', INTID_COLUMN_TYPE, nullable=True, autoincrement=False)
-	blog_length = Column('blog_length', Integer, nullable=True, autoincrement=False)
-	blog_id = Column('blog_id', Integer, Sequence('blog_seq'), index=True, nullable=False, primary_key=True)
+    blog_ds_id = Column('blog_ds_id', INTID_COLUMN_TYPE,
+                        nullable=True, autoincrement=False)
 
-	@property
-	def Blog(self):
-		id_utility = component.getUtility( IAnalyticsIntidIdentifier )
-		return id_utility.get_object( self.blog_ds_id )
+    blog_length = Column('blog_length', Integer,
+                         nullable=True, autoincrement=False)
+
+    blog_id = Column('blog_id', Integer, Sequence('blog_seq'),
+                     index=True, nullable=False, primary_key=True)
+
+    @property
+    def Blog(self):
+        id_utility = component.getUtility(IAnalyticsIntidIdentifier)
+        return id_utility.get_object(self.blog_ds_id)
+
 
 class BlogsViewed(Base, BaseViewMixin, BlogMixin, TimeLengthMixin):
 
-	__tablename__ = 'BlogsViewed'
+    __tablename__ = 'BlogsViewed'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'blog_id', 'timestamp'),
-	)
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'blog_id', 'timestamp'),
+    )
+
 
 class BlogCommentsCreated(Base, CommentsMixin, BlogMixin, RatingsMixin):
-	__tablename__ = 'BlogCommentsCreated'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('comment_id'),
-	)
+    __tablename__ = 'BlogCommentsCreated'
 
-	_file_mime_types = relationship( 'BlogCommentsUserFileUploadMimeTypes', lazy="select" )
+    __table_args__ = (
+        PrimaryKeyConstraint('comment_id'),
+    )
 
-	@property
-	def FileMimeTypes(self):
-		result = {}
-		mime_types = self._file_mime_types
-		if mime_types:
-			for mime_type in mime_types:
-				result[mime_type.mime_type] = mime_type.count
-		return result
+    _file_mime_types = relationship('BlogCommentsUserFileUploadMimeTypes', 
+                                    lazy="select")
+
+    @property
+    def FileMimeTypes(self):
+        result = {}
+        mime_types = self._file_mime_types
+        for mime_type in mime_types or ():
+            result[mime_type.mime_type] = mime_type.count
+        return result
+
 
 class BlogFavorites(Base, BaseTableMixin, BlogMixin, CreatorMixin):
-	__tablename__ = 'BlogFavorites'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'blog_id'),
-	)
+    __tablename__ = 'BlogFavorites'
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'blog_id'),
+    )
+
 
 class BlogLikes(Base, BaseTableMixin, BlogMixin, CreatorMixin):
-	__tablename__ = 'BlogLikes'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'blog_id'),
-	)
+    __tablename__ = 'BlogLikes'
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'blog_id'),
+    )
+
 
 class BlogCommentMixin(object):
 
-	@declared_attr
-	def comment_id(cls):
-		return Column('comment_id',
-					  INTID_COLUMN_TYPE,
-					  ForeignKey("BlogCommentsCreated.comment_id"),
-					  nullable=False,
-					  index=True)
+    @declared_attr
+    def comment_id(self):
+        return Column('comment_id',
+                      INTID_COLUMN_TYPE,
+                      ForeignKey("BlogCommentsCreated.comment_id"),
+                      nullable=False,
+                      index=True)
+
 
 class BlogCommentsUserFileUploadMimeTypes(Base, FileMimeTypeMixin, BlogCommentMixin):
 
-	__tablename__ = 'BlogCommentsUserFileUploadMimeTypes'
+    __tablename__ = 'BlogCommentsUserFileUploadMimeTypes'
 
-	blog_comment_file_upload_mime_type_id = Column('blog_comment_file_upload_mime_type_id',
-											Integer,
-											Sequence('blog_comment_file_upload_seq'),
-											index=True,
-					 						nullable=False,
-					 						primary_key=True)
+    blog_comment_file_upload_mime_type_id = Column('blog_comment_file_upload_mime_type_id',
+                                                   Integer,
+                                                   Sequence('blog_comment_file_upload_seq'),
+                                                   index=True,
+                                                   nullable=False,
+                                                   primary_key=True)
+
 
 class BlogCommentFavorites(Base, BaseTableMixin, BlogCommentMixin, CreatorMixin):
 
-	__tablename__ = 'BlogCommentFavorites'
+    __tablename__ = 'BlogCommentFavorites'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'comment_id'),
-	)
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'comment_id'),
+    )
+
 
 class BlogCommentLikes(Base, BaseTableMixin, BlogCommentMixin, CreatorMixin):
 
-	__tablename__ = 'BlogCommentLikes'
+    __tablename__ = 'BlogCommentLikes'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'comment_id'),
-	)
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'comment_id'),
+    )
