1fadb454d6a9974da0791945f98de97cf6c4a99c
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-20 20:57:02 +0000

    add an interface for the analytics database
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_database/trunk@115634 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_database/interfaces.py
+++ src/nti/analytics_database/interfaces.py
@@ -4,52 +4,61 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 from zope import interface
 
+class IAnalyticsDatabase(interface.Interface):
+    """
+    An utility interface for the Analytics database
+    """
+
+
 class IAnalyticsDSIdentifier(interface.Interface):
-	"""
-	A utility that gets dataserver identities for objects
-	and dataserver objects from identities.
-	"""
+    """
+    A utility that gets dataserver identities for objects
+    and dataserver objects from identities.
+    """
 
-	def get_id(self, obj):
-		"""
-		For the given dataserver object, return a unique
-		dataserver identifier.
-		"""
+    def get_id(self, obj):
+        """
+        For the given dataserver object, return a unique
+        dataserver identifier.
+        """
 
-	def get_object(self, obj_id):
-		"""
-		For the given dataserver identifier, return a
-		dataserver object.
-		"""
+    def get_object(self, obj_id):
+        """
+        For the given dataserver identifier, return a
+        dataserver object.
+        """
+
 
 class IAnalyticsNTIIDIdentifier(IAnalyticsDSIdentifier):
-	"""
-	``IAnalyticsDSIdentifier`` for NTIIDs.
-	"""
+    """
+    ``IAnalyticsDSIdentifier`` for NTIIDs.
+    """
+
 
 class IAnalyticsIntidIdentifier(IAnalyticsDSIdentifier):
-	"""
-	``IAnalyticsDSIdentifier`` for ds intids.
-	"""
+    """
+    ``IAnalyticsDSIdentifier`` for ds intids.
+    """
+
 
 class IAnalyticsRootContextIdentifier(IAnalyticsDSIdentifier):
-	"""
-	``IAnalyticsDSIdentifier`` for root context objects, which may
-	resolve into courses or books.
-	"""
+    """
+    ``IAnalyticsDSIdentifier`` for root context objects, which may
+    resolve into courses or books.
+    """
+
 
 class IAnalyticsRootContextResolver(interface.Interface):
-	"""
-	Marker interface to resolve root context objects
-	"""
+    """
+    Marker interface to resolve root context objects
+    """
 
-	def __call__(rc_id):
-		pass
-
+    def __call__(rc_id):
+        pass
