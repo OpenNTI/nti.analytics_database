38a97c89500099f5dddb42ad8f7e359c4f0a80c1
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-20 20:59:08 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_database/trunk@115636 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_database/profile_views.py
+++ src/nti/analytics_database/profile_views.py
@@ -4,7 +4,7 @@
 .. $Id$
 """
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -22,31 +22,37 @@ from nti.analytics_database import Base
 from nti.analytics_database.meta_mixins import BaseViewMixin
 from nti.analytics_database.meta_mixins import TimeLengthMixin
 
+
 class EntityProfileMixin(BaseViewMixin, TimeLengthMixin):
 
-	@declared_attr
-	def target_id(cls):
-		return Column('target_id', Integer, ForeignKey("Users.user_id"), 
-					  index=True, nullable=False)
+    @declared_attr
+    def target_id(cls):
+        return Column('target_id', Integer, ForeignKey("Users.user_id"),
+                      index=True, nullable=False)
+
 
 class EntityProfileViews(Base, EntityProfileMixin):
-	__tablename__ = 'EntityProfileViews'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
-	)
+    __tablename__ = 'EntityProfileViews'
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
+    )
+
 
 class EntityProfileActivityViews(Base, EntityProfileMixin):
 
-	__tablename__ = 'EntityProfileActivityViews'
+    __tablename__ = 'EntityProfileActivityViews'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
-	)
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
+    )
+
 
 class EntityProfileMembershipViews(Base, EntityProfileMixin):
-	__tablename__ = 'EntityProfileMembershipViews'
 
-	__table_args__ = (
-		PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
-	)
+    __tablename__ = 'EntityProfileMembershipViews'
+
+    __table_args__ = (
+        PrimaryKeyConstraint('user_id', 'target_id', 'timestamp'),
+    )
