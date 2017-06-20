6ff89af6a7caa7716ff3c6cf883866209593b1a4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-20 21:14:52 +0000

    update tests
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.analytics_database/trunk@115640 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/analytics_database/tests/__init__.py
+++ src/nti/analytics_database/tests/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -11,39 +11,47 @@ import os
 import unittest
 import importlib
 
-from sqlalchemy.pool import StaticPool
-from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine as sqlalchemy_create_engine
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import scoped_session
+
+from sqlalchemy.pool import StaticPool
 
 from nti.analytics_database import Base
 
-# import in order to create tables
+
 def _import_db_modules():
+    """
+    Import modules to create database
+    """
     path = os.path.join(os.path.dirname(__file__), '..')
-    files = [os.path.splitext(f)[0] for f in os.listdir(path) 
-             if os.path.isfile(os.path.join(path,f)) and f.endswith('.py') ]
+    files = [os.path.splitext(f)[0] for f in os.listdir(path)
+             if os.path.isfile(os.path.join(path, f)) and f.endswith('.py')]
     for name in files:
         name = "nti.analytics_database.%s" % name
         importlib.import_module(name)
 _import_db_modules()
 del _import_db_modules
 
-def create_engine(dburi='sqlite://', pool_size=30, max_overflow=10, pool_recycle=300):    
+
+def create_engine(dburi='sqlite://', pool_size=30, max_overflow=10, pool_recycle=300):
     try:
         if dburi == 'sqlite://':
             result = sqlalchemy_create_engine(dburi,
-                                              connect_args={'check_same_thread':False},
+                                              connect_args={'check_same_thread': False},
                                               poolclass=StaticPool)
 
         else:
-            result = sqlalchemy_create_engine( dburi,
-                                               pool_size=pool_size,
-                                               max_overflow=max_overflow,
-                                               pool_recycle=pool_recycle)
+            result = sqlalchemy_create_engine(dburi,
+                                              pool_size=pool_size,
+                                              max_overflow=max_overflow,
+                                              pool_recycle=pool_recycle)
     except TypeError:
         # SQLite does not use pooling anymore.
         result = sqlalchemy_create_engine(dburi)
     return result
+
 
 def create_sessionmaker(engine, autoflush=False, twophase=False):
     result = sessionmaker(bind=engine,
@@ -51,11 +59,13 @@ def create_sessionmaker(engine, autoflush=False, twophase=False):
                           twophase=twophase)
     return result
 
+
 def create_session(sessionmaker):
     return scoped_session(sessionmaker)
 
+
 class AnalyticsDatabaseTest(unittest.TestCase):
-    
+
     def setUp(self):
         self.engine = create_engine()
         self.metadata = getattr(Base, 'metadata')
--- src/nti/analytics_database/tests/test_database.py
+++ src/nti/analytics_database/tests/test_database.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -11,6 +11,7 @@ from hamcrest import has_length
 from hamcrest import assert_that
 
 from nti.analytics_database.tests import AnalyticsDatabaseTest
+
 
 class TestDatabase(AnalyticsDatabaseTest):
 
