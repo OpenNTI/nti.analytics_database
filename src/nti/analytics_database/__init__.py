d6e9dd9cf92fd932da9001502c9aa6bc6193565b
Josh Zuech <josh.zuech@nextthought.com>
2020-01-15 13:54:05 -0600

    Make sure load table defs

--- src/nti/analytics_database/__init__.py
+++ src/nti/analytics_database/__init__.py
@@ -30,3 +30,11 @@ INTID_COLUMN_TYPE = BigInteger
 #: The character used to separate context path elements on storage.
 #: '/' is illegal in ntiid strings
 CONTEXT_PATH_SEPARATOR = '/'
+
+# Init so these exist in our Base
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__(module[:-3], locals(), globals())
+del module
